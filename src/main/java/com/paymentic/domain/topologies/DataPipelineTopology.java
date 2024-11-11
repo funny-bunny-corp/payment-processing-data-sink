fad30a40abf9a0b8cfb3af376a521b797dc380a3
claudioed <claudioed.oliveira@gmail.com>
2024-11-11 07:56:41 -0300

    add env variable for kafka topic

--- src/main/java/com/paymentic/domain/topologies/DataPipelineTopology.java
+++ src/main/java/com/paymentic/domain/topologies/DataPipelineTopology.java
@@ -3,6 +3,7 @@ package com.paymentic.domain.topologies;
 import io.cloudevents.CloudEvent;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
+import java.util.List;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -11,16 +12,18 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.processor.TopicNameExtractor;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class DataPipelineTopology {
 
-  private final static String SOURCE_TOPIC = "payment-processing";
+  @ConfigProperty(name = "quarkus.kafka-streams.topics")
+  List<String> sourceTopic;
 
   @Produces
   public Topology totalOrderSellerPerDay(Serde<CloudEvent> ceSerde) {
     var builder = new StreamsBuilder();
-    final KStream<String, CloudEvent> paymentProcessingStream = builder.stream(SOURCE_TOPIC,
+    final KStream<String, CloudEvent> paymentProcessingStream = builder.stream(this.sourceTopic,
         Consumed.with(Serdes.String(), ceSerde));
     final TopicNameExtractor<String, CloudEvent> topicName = (key, value, recordContext) -> value.getType() + "-sink"  ;
     paymentProcessingStream.to(topicName, Produced.with(Serdes.String(), ceSerde));
